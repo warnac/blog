5a150959342104e959d632f39595f29e5a28f7f8
warnaagung <warnaagung@gmail.com>
2026-06-18 06:23:24 +0700

    Bangun Fitur Daftar Post Bagian Fitur Delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -88,6 +88,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
     }
 }
